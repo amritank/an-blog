a5e2ed07ed093fdcc77b719a930ebc92b3c550f5
amritank <amrita.sasi.nair@gmail.com>
2024-07-03 09:58:28 -0700

    initial commit

---
+++ assets/js/blog.js
@@ -0,0 +1,44 @@
+const backButtonEl = document.querySelector("#backSubmitButton");
+const mainEl = document.querySelector('#main-container')
+
+
+// <----- Callback definitions ---->
+function listBlogPostsFromLocalStorage() {
+    const blogEntries = loadFromLocalStorage();
+    if (blogEntries) {
+        console.log(blogEntries);
+        blogEntries.forEach((post) => {
+            // fetch the variables
+            const uname = post.username;
+            const title = post.title;
+            const content = post.content;
+
+            console.log(`Printing blog entry with title: ${title} from username: ${uname}`);
+
+            // create the div container
+            const divEl = document.createElement("div");
+            divEl.classList.add('blog-post');
+            const h4El = document.createElement("h4");
+            h4El.classList.add('post-header', 'post-sections');
+            h4El.textContent = title;
+            const pContentEl = document.createElement("p");
+            pContentEl.classList.add('post-sections', 'post-content');
+            pContentEl.textContent = content;
+            const pUnameEl = document.createElement("p");
+            pUnameEl.classList.add('post-author');
+            pUnameEl.textContent = "Posted by: " + uname;
+            divEl.append(h4El, pContentEl, pUnameEl);
+            mainEl.appendChild(divEl);
+        });
+    }
+
+}
+
+function navigateToLandingPage() {
+    window.location.href = "./index.html";
+}
+
+
+backButtonEl.addEventListener("click", navigateToLandingPage);
+
+window.onload = listBlogPostsFromLocalStorage;
---
+++ assets/js/script.js
@@ -0,0 +1,84 @@
+const submitEl = document.querySelector("#submitButton");
+const usernameEl = document.querySelector("#username");
+const titleEl = document.querySelector("#title");
+const contentEl = document.querySelector("#content")
+const pEl = document.querySelector("#error-msg");
+const formEls = document.querySelectorAll('.form-data-elements');
+const bodyEl = document.querySelector('body');
+
+
+// <---- creating the object to store blog entries ---->
+let blogEntries = [];
+
+
+// <------ Callback definitions ----->
+function loadEntries() {
+    blogEntries = loadFromLocalStorage();
+}
+function storeToLocalStorage() {
+    const blog = {
+        username: usernameEl.value,
+        title: title.value,
+        content: contentEl.value
+    }
+    console.log("Created blog entry: ");
+    console.log(blog);
+
+    console.log("Push blog entry to the array");
+    blogEntries.push(blog);
+
+    console.log("Blog entries so far");
+    console.log(blogEntries);
+
+    console.log("Store blog entries to localstorage");
+    localStorage.setItem("blog", JSON.stringify(blogEntries));
+}
+
+function resetFields() {
+    usernameEl.textContent = "";
+    titleEl.textContent = "";
+    contentEl.textContent = "";
+}
+
+// Function invoked on form submission.
+function submitForm(event) {
+    console.log("Turn off form refresh");
+    event.preventDefault();
+
+    console.log("Verify form fields are not empty");
+    if (usernameEl.value === "" ||
+        titleEl.value === "" ||
+        contentEl.value === "") {
+        pEl.textContent = "Please fill in all fields in the form";
+        pEl.setAttribute("class", "show-error-msg");
+        return;
+    }
+    storeToLocalStorage();
+
+    window.location.href = "./blog.html";
+}
+
+// Function invoked when form is clicked because we want to clear the error msg
+// if it is displayed from previous attempts.
+function clearErrorMessage() {
+    pEl.textContent = "";
+    pEl.setAttribute("class", "hidden");
+    // resetFields(); TODO: check later why this does not reset the text on the fields 
+}
+
+
+// <------ Attach all event listeners ----->
+// Loop thru each input box in the form and if the user click on any of the input boxes clear the 
+// error message.
+for (const ele of formEls) {
+    ele.addEventListener('click', clearErrorMessage)
+}
+
+// Submit form event listener.
+// TODO: how about trying submit event from form eleemnt
+submitEl.addEventListener('click', submitForm);
+
+
+
+// method called when page loads for first time:
+window.onload = loadEntries;
